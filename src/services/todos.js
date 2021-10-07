dcf5e58a4bca09a8a8a764040bda2bf5b238217c
Saurajit Bhuyan <saurajitb@gmail.com>
2021-10-07 14:45:02 +0530

    Added operation for APIs in todoService

--- src/services/todos.js
+++ src/services/todos.js
@@ -4,7 +4,28 @@ const todoService = {
 
   getAll: (page = 1, limit = 10) => {
     return fetch(`${API_URL}?p=${page}&l=${limit}`);
+  },
+
+  addTodo: (todo) => {
+    return fetch(API_URL, {
+      method: 'POST',
+      body: todo
+    })
+  },
+
+  removeTodo: (id) => {
+    return fetch(`${API_URL}/${id}`, {
+      method: 'DELETE'
+    })
+  },
+
+  changeStatus: (id, status = false) => {
+    return fetch(`${API_URL}/${id}`, {
+      method: 'PUT',
+      body: {status}
+    });
   }
+
 }
 
 export default todoService;
